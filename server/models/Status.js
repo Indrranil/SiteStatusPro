e076f8ca87be42b646113dde446734a34c50f623
Indrranil Pawar <112892653+Indrranil@users.noreply.github.com>
2024-07-31 11:54:26 +0530

    Update Status.js

---
+++ server/models/Status.js
@@ -0,0 +1,20 @@
+// server/models/Status.js
+
+const statuSchema = new mongoose.Schema ({
+	website: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		required: true,
+	},
+	timestamp: {
+		type: Date,
+		default: Date.now,
+	},
+}),
+
+const Status = mongoose.model('Status',statusSchema);
+
+module.exports = Status;
